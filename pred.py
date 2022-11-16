43bf8109f37460681e0f8f1e5bb8445b5b83de03
CeruleanHokori <razani.ziad0@gmaiL.com>
2022-11-16 16:24:55 +0100

    Adjusted pred.py

--- pred.py
+++ pred.py
@@ -4,9 +4,7 @@ from data_proc import decoder
 from transformers import BertTokenizer
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = torch.load("sentiment_v0")
 
-new_sentence = "I've waited in a queue for hours"
 
 tokenizer = tokenizer = BertTokenizer.from_pretrained(
     'bert-base-uncased',
@@ -22,23 +20,24 @@ def preprocessing(input_text, tokenizer):
                         return_tensors = 'pt'
                    )
 # We need Token IDs and Attention Mask for inference on the new sentence
-test_ids = []
-test_attention_mask = []
+def predict(sentence,model=None):
+  if model == None:
+    model = torch.load("sentiment_v0")
+  test_ids = []
+  test_attention_mask = []
 
-# Apply the tokenizer
-encoding = preprocessing(new_sentence, tokenizer)
+  # Apply the tokenizer
+  encoding = preprocessing(sentence, tokenizer)
 
-# Extract IDs and Attention Mask
-test_ids.append(encoding['input_ids'])
-test_attention_mask.append(encoding['attention_mask'])
-test_ids = torch.cat(test_ids, dim = 0)
-test_attention_mask = torch.cat(test_attention_mask, dim = 0)
+  # Extract IDs and Attention Mask
+  test_ids.append(encoding['input_ids'])
+  test_attention_mask.append(encoding['attention_mask'])
+  test_ids = torch.cat(test_ids, dim = 0)
+  test_attention_mask = torch.cat(test_attention_mask, dim = 0)
 
-# Forward pass, calculate logit predictions
-with torch.no_grad():
-  output = model(test_ids.to(device), token_type_ids = None, attention_mask = test_attention_mask.to(device))
+  # Forward pass, calculate logit predictions
+  with torch.no_grad():
+    output = model(test_ids.to(device), token_type_ids = None, attention_mask = test_attention_mask.to(device))
 
-prediction = decoder(np.argmax(output.logits.cpu().numpy()).flatten().item())
-
-print('Input Sentence: ', new_sentence)
-print('Predicted Class: ', prediction)
+  prediction = decoder(np.argmax(output.logits.cpu().numpy()).flatten().item())
+  return prediction
